cc9cdda345469461ef374908cecfec872e6cd4d6
cha1ra <cha1ra.garnet@gmail.com>
2022-01-28 14:48:52 +0900

    Laravel Breeze セットアップ

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TweetController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return view('hello');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/tweets', [TweetController::class, 'index']);
-
+require __DIR__.'/auth.php';
