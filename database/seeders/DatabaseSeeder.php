c9438b3fe7fae2668ca12cd4624424031c92b769
cha1ra <cha1ra.garnet@gmail.com>
2022-02-02 05:28:25 +0900

    fix: init seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,23 +16,5 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-
-        User::create([
-            'name' => 'テストユーザ',
-            'email' => 'test@example.com',
-            'password' => bcrypt('password'),
-        ]);
-
-        Tweet::create([
-            'message' => 'Seederからツイート作成した件'
-        ]);
-
-        Tweet::create([
-            'message' => 'phpMyAdminからではない件'
-        ]);
-        
-        Tweet::create([
-            'message' => '便利な件'
-        ]);
     }
 }
