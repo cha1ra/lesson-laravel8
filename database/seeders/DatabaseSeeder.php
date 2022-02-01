09236859dd818581a743073b41b03a1562979f51
cha1ra <cha1ra.garnet@gmail.com>
2022-02-02 05:37:24 +0900

    Seederの作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Tweet;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,27 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'テストユーザ',
+            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        User::create([
+            'name' => 'ぼやき太郎',
+            'email' => 'boyaki@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        Tweet::create([
+            'message' => 'テストユーザによる投稿',
+            'user_id' => 1
+        ]);
+
+        Tweet::create([
+            'message' => 'ぼやき太郎による投稿',
+            'user_id' => 2
+        ]);
     }
 }
