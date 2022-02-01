1702e368de7b04c62554d16eb3d9bc447efd1249
cha1ra <cha1ra.garnet@gmail.com>
2022-02-02 07:06:29 +0900

    tweets.blade.php に タグ選択フォームを追記

--- resources/views/tweets.blade.php
+++ resources/views/tweets.blade.php
@@ -11,6 +11,19 @@
                                 <textarea class="form-control" id="text-area" rows="3" name="message" placeholder="ぼやきを入力"></textarea>
                             </div>
                         </div>
+                        <!-- タグづけ用選択チェックボックス ここから -->
+                        <div class="mb-2">
+                            <p class="mb-1 text-gray-400 font-weight-bold" style="font-size: 0.8rem;">タグを選択</p>
+                            <div class="form-outline mb-2">
+                                @foreach($tags as $tag)
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="checkbox" id="tag-checkbox{{$tag->id}}" name="tags[]" value="{{$tag->id}}" />
+                                        <label class="form-check-label" for="tag-checkbox2">{{$tag->name}}</label>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                         <!-- タグづけ用選択チェックボックス ここまで -->
                         <button
                             type="submit"
                             class="btn btn-primary btn-lg btn-block shadow-0 font-weight-bold"
