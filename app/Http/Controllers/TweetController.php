e37c1c02b05d8e604e0116a4faf4514474daa641
cha1ra <cha1ra.garnet@gmail.com>
2022-02-04 12:53:54 +0900

    DELETE /tweets/{id}/ エンドポイントの作成

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -117,8 +117,5 @@ class TweetController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
-    {
-        //
-    }
+
 }
