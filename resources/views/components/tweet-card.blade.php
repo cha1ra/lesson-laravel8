76a7f01b5c245e14591a39a8945021650557994d
cha1ra <cha1ra.garnet@gmail.com>
2022-02-04 12:34:37 +0900

    twwet-card に編集・削除ボタンを追加

--- resources/views/components/tweet-card.blade.php
+++ resources/views/components/tweet-card.blade.php
@@ -4,6 +4,18 @@
             <span class="font-weight-bold mr-2">{{ $tweet->user->name }}</span>
             <span style="font-size: 0.8rem;">{{ $tweet->created_at }}</span>
         </p>
+        <div class="d-flex" style="z-index:2">
+            <a href="/tweets/{{$tweet->id}}/edit"  class="btn btn-floating shadow-0" >
+                <i class="fas fa-edit fa-lg"></i>
+            </a>
+            <form action="/tweets/{{$tweet->id}}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-floating shadow-0">
+                    <i class="fas fa-trash fa-lg"></i>
+                </button>
+            </form>
+        </div>
     </div>
     <p class="card-text">
         {{ $tweet->message }}
